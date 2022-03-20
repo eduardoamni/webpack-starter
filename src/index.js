1c68523e3c75c8be37809934e8d9f89047ebfc86
Eduardo <eduardoamni@gmail.com>
2022-03-20 11:26:59 +0100

    cambio mensaje en readme.md 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-// va a buscar un archivo JS no hace falta poner el '.js'
-import {saludar} from './assets/js/componentes';
-import './assets/css/styles.css';
 
-const nombre = 'Eduardo';
-
-saludar (nombre);
